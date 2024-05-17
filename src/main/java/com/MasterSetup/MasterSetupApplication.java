757a769251b4e2a6ab676920a18c54bec9d81655
Lilyppu <lily@ppu.co.id>
2024-05-17 10:26:42 +0700

    first commit

---
+++ src/main/java/com/MasterSetup/MasterSetupApplication.java
@@ -0,0 +1,13 @@
+package com.MasterSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSetupApplication.class, args);
+	}
+
+}
