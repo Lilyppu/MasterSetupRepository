f59b7f1d970fe189999fe8c3f80c288599006f3d
andirhl <andi@ppu.co.id>
2024-05-28 11:32:15 +0700

    update 20240528 - by am

---
+++ src/main/java/Models/EntityMaster.java
@@ -0,0 +1,101 @@
+package Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ENTITY_MASTER")
+public class EntityMaster {
+	
+	@Id
+	@Column(name="ENTY_TYPE",length=2,  nullable = false)
+	private String entyType;
+	
+	@Column(name="ENTY_CURRENCY",length=5)
+	private String entyCurrency;
+	
+	@Column(name="ENTY_NO",length=10)
+	private String entyNo;
+	
+	@Column(name="ENTY_NAME",length=100)
+	private String entyName;
+	
+	@Column(name="ENTY_SHORT_NAME",length=20)
+	private String entyShortName;
+	
+	@Column(name="ENTY_ADDRESS",length=200)
+	private String entyAddress;
+	
+	@Column(name="ENTY_CITY",length=50)
+	private String entyCity;
+	
+	@Column(name="ENTY_PROVINCE",length=50)
+	private String entyProvince;
+	
+	@Column(name="ENTY_COUNTY",length=50)
+	private String entyCountry;
+	
+	@Column(name="ENTY_ZIPCODE",length=8)
+	private String entyZipCode;
+	
+	@Column(name="ENTY_TAXID",length=20)
+	private String entyTaxId;
+	
+	@Column(name="ENTY_SB_NO",length=30)
+	private String entySbNo;
+	
+	@Column(name="ENTY_SB_DATE")
+	private Date entySbDate;
+	
+	@Column(name="ENTY_EMAIL", length=50)
+	private String entyEmail;
+	
+	@Column(name="ENTY_PHONE", length=20)
+	private String entyPhone;
+	
+	@Column(name="COMP_FAX", length=20)
+	private String entyFax;
+	
+	@Column(name="ENTY_PHONE2", length=20)
+	private String entyPhone2;
+	
+	@Column(name="ENTY_FAX2", length=20)
+	private String entyFax2;
+	
+	@Column(name="ENTY_POBOX", length=10)
+	private String entyPoBox;
+	
+	@Column(name="ENTY_URI", length=20)
+	private String entyURI;
+	
+	@Column(name="ENTY_WEBSITE", length=100)
+	private String entyWebSite;
+	
+	@Column(name="ENTY_HANDPHONE", length=20)
+	private String entyHandPhone;
+	
+	@Column(name="ENTY_IP_PORT", length=20)
+	private String entyIpPort;
+	
+	@Column(name="ENTY_ENABLE", length=1)
+	private String entyEnbale;
+	
+	 @Column(name="AC_USER_CRE",length=20)
+	 private String fmFPosUserCrea;
+	 
+	 @Column(name="AC_DATE_CRE")
+	 private Date fmFPosDateCrea;
+	 
+	 @Column(name="AC_USER_UPD",length=20)
+	 private String fmFPosUserUpd;
+	 
+	 @Column(name="AC_DATE_UPD")
+	 private Date fmFPosDateUpd;
+}
---
+++ src/main/java/Models/OfficeMaster.java
@@ -0,0 +1,74 @@
+package Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "GL_OFF_MSTR")
+public class OfficeMaster {
+	@Id
+	@Column(name="OF_CODE",length=20,  nullable = false)
+	private String offCode;
+	
+	@Column(name="OF_DESC",length=50)
+	private String offDesc;
+	
+	@Column(name="OF_TYPE",length=20)
+	private String offType;
+	
+	@Column(name="OF_UPLEVEL_CODE",length=20)
+	private String offUpLevelCode;
+	
+	@Column(name="OF_PIC",length=50)
+	private String offPICe;
+	
+	@Column(name="OF_ADDR",length=200)
+	private String offAddr;
+	
+	@Column(name="OF_CITY",length=50)
+	private String offCity;
+	
+	@Column(name="OF_PROV",length=50)
+	private String offProv;
+	
+	@Column(name="OF_NATION",length=50)
+	private String offNation;
+	
+	@Column(name="OF_ZIP",length=10)
+	private String offZip;
+	
+	@Column(name="OF_PHONE",length=30)
+	private String offPhone;
+	
+	@Column(name="OF_FAX",length=30)
+	private String offFax;
+	
+	@Column(name="OF_EMAIL",length=30)
+	private String offEmail;
+	
+	@Column(name="OF_NPWP",length=30)
+	private String offNpwp;
+	
+	@Column(name="AC_STATUS",length=2)
+	private String offStatus;
+	
+	 @Column(name="AC_USER_CRE",length=20)
+	 private String fmFPosUserCrea;
+	 
+	 @Column(name="AC_DATE_CRE")
+	 private Date fmFPosDateCrea;
+	 
+	 @Column(name="AC_USER_UPD",length=20)
+	 private String fmFPosUserUpd;
+	 
+	 @Column(name="AC_DATE_UPD")
+	 private Date fmFPosDateUpd;
+
+}
